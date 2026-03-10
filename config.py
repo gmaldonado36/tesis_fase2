2f53e6904e674685e4d1c87c3b8fc641d850c095
Gerardo Maldonado <gmaldonadopoinsot@gmail.com>
2026-03-09 22:59:43 -0600

    update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOTAL_IMAGES = 30
+TOTAL_IMAGES = 60
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FASE1_FOLDER = os.path.join(BASE_DIR, "data", "fase_1")
 FASE2_FOLDER = os.path.join(BASE_DIR, "data", "fase_2")
