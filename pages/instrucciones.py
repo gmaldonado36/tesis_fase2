d33c03a6132b90355d821a4a5370a990f123f967
Gerardo Maldonado <gmaldonadopoinsot@gmail.com>
2026-03-09 22:52:42 -0600

    final changes

--- pages/instrucciones.py
+++ pages/instrucciones.py
@@ -3,9 +3,11 @@ import streamlit as st
 def run():
     st.title("Información General")
 
-    st.write("Bienvenidos a la prueba.")
-    st.write("Esta prueba consta de **2 fases.**")
+    st.write("Bienvenido(a) a la prueba.")
+    st.write("Esta prueba consta de **2 fases**.")
     st.write("Al **finalizar** cada fase, podrás tomar un breve **descanso** si lo deseas.")
+    st.write("En cada fase verás una serie de **imágenes** que puede que hayas visto en la prueba anterior o que no.")
+    st.write("Tu misión es identificar correctamente **cuáles imágenes ya viste y cuáles no**.")
     st.write("Cuando estés listo(a) para continuar, podrás iniciar la siguiente fase haciendo clic en el botón correspondiente.")
 
     if st.button("Avanzar", type="primary"):
